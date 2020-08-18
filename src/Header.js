ea10d4db6a92b4119951a9a8b0d61c3c99f12881
Joshua Sevy <joshuasevy@outlook.com>
2020-08-18 17:37:08 -0600

    Add header component and stylesheet

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+import React from 'react';
+
+const Header = () => {
+  return (
+    <section className="header-container">
+    <h2 className="header-title">Rancid Tomatillos</h2>
+      <article className="header-login">
+        <button>LogIn</button>
+      </article>
+    </section>
+  )
+}
+
+export default Header;
