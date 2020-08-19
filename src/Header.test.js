ec676b193b615e87d03eda2a9aea69cf00a5e197
Joshua Sevy <joshuasevy@outlook.com>
2020-08-18 21:06:50 -0600

    Add test files for header and login

---
+++ src/Header.test.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import React from 'react';
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+test('practice test', () => {
+  expect(true).toBe(true)
+})
+
+test('renders button on header', () => {
+  const { getByText } = render(<Header />);
+  const linkElement = getByText(/Rancid Tomatillos/i);
+  expect(linkElement).toBeInTheDocument();
+});
