6bace8d541af5556f71a54df46aaaf2fb4272332
Ben McClung <aurumvalian@gmail.com>
2020-08-19 17:24:08 -0600

    Style movie page

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -9,14 +9,16 @@ function Movie(props) {
 
   return (
     <section className="movie-page" style={style}>
-      <button className="back-button"></button>
-      <h3>{props.movie.title}</h3>
-      <p>Description: {props.movie.overview}</p>
-      <p>
-        Genres: {props.movie.genres} <br />
-        Length: {props.movie.runtime} Minutes <br />
-        Average Rating: {props.movie["average_rating"]} Tomatillos
-      </p>
+      <button className="back-button">Back</button>
+      <article className="movie-description">
+        <h3>{props.movie.title}</h3>
+        <p>Description: {props.movie.overview}</p>
+        <p>
+          Genres: {props.movie.genres} <br />
+          Length: {props.movie.runtime} Minutes <br />
+          Average Rating: {props.movie["average_rating"]} Tomatillos
+        </p>
+      </article>
     </section>
   )
 }
