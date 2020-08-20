4061d7c81bcde0e61396c92b64b9c0e3db740579
Joshua Sevy <joshuasevy@outlook.com>
2020-08-20 16:13:39 -0600

    Update bug in card component

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -8,7 +8,8 @@ function Card(props) {
   return (
     <div className="Card-block">
       <article 
-        className="Card">
+        className="Card" 
+        id={props.id}>
         <img src={props.poster} alt="Movie Poster" />
         <h3>{props.title}</h3>
         <p>Average Rating: {props.avgRating}</p>
