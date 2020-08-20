02994e2dab45bf3de43e607cbca5dbfce96d14e8
Joshua Sevy <joshuasevy@outlook.com>
2020-08-20 15:51:16 -0600

    Add testing for movie component

--- src/Movie/Movie.test.js
+++ src/Movie/Movie.test.js
@@ -1,11 +1,30 @@
 import Movie from './Movie';
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 
-// test('checks for text on posters', () => {
-//   const { getByText } = render(<Movie />);
-//   const linkElement = getByText(/Average Rating:/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
+describe('Movie Component', () => {
+  it('should render Movie Component correctly', () => {
+
+    render(
+      <Movie
+        movie={[]}
+      />)
+
+    screen.debug()
+
+
+    const button = screen.getByRole('button');
+    const movieLength = screen.getByText(/length:/i);
+    const ratings = screen.getByText(/average rating/i)
+    const description = screen.getByText(/description/i);
+    const genres = screen.getByText(/genres/i);
+
+    expect(description).toBeInTheDocument();
+    expect(genres).toBeInTheDocument();
+    expect(movieLength).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
+    expect(ratings).toBeInTheDocument();
+  })
+})
