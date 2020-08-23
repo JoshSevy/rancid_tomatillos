3c877454d97f8d34cc5163beb34a77aa3ad904c5
Joshua Sevy <joshuasevy@outlook.com>
2020-08-22 23:39:11 -0600

    Add testing for error component

--- src/Card/Card.test.js
+++ src/Card/Card.test.js
@@ -2,18 +2,21 @@ import Card from './Card';
 import React from 'react';
 import { render,  screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 
-describe.skip('Card Component', () => {
+describe('Card Component', () => {
   
   it('should have correct content when rendered', () => {
     render(
-      <Card 
-        poster="image-file.jpg"
-        id={2}
-        title="Inception"
-        avgRating= "9.9"
-      />
+      <BrowserRouter>
+        <Card 
+          poster="image-file.jpg"
+          id={2}
+          title="Inception"
+          avgRating= "9.9"
+        />
+      </BrowserRouter>
     )
   
   const title = screen.getByText('Inception');
